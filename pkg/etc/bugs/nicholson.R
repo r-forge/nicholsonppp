873791478ffd9e03927db7a4421e944968ef4691
tdhock (Toby Dylan Hocking) <tdhock5@gmail.com>
2009-08-10 15:22:27 +0000

    nicholson model comparisons between winbugs and fortran
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/nicholsonppp@5 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/etc/bugs/nicholson.R
+++ pkg/etc/bugs/nicholson.R
@@ -1,6 +1,6 @@
 library(R2WinBUGS)
 ids <- "2009-06-30-15-56-18"
-result.dirs <- paste("results/",ids,"/",sep='')
+result.dirs <- paste("~/projects/sim/results/",ids,"/",sep='')
 names(result.dirs) <- ids
 source("db.R") # for read.res.tables
 r <- read.res.tables(result.dirs,c(Y="Y_SIM_HMN",N="N_SIM_HMN"))
@@ -14,10 +14,38 @@ rownames(params) <- ids
 ## Variables in model:
 ## data: I J Y N
 ## init: alpha p tau c
-init <- function(){
-  list(p=
-       c=)
-}
 inits <- list(list(p=rep(0.5,r$I),c=rep(0.1,r$J))) #,alpha=as.matrix(Y/N))
 vars <- c("alpha","p","c")
 res <- bugs(r,inits,vars,"/home/thocking/projects/bugs/nicholson.bugs",n.chains=1,n.iter=500,n.burnin=100)
+R <- read.res.tables(result.dirs)
+est <- data.frame(simulated=R$pi.sim[,1],
+                  fortran=R$pi.est$MOY,
+                  winbugs=res$mean$p,
+                  locus=factor(1:r$I),
+                  ppp=R$ppp$PPPVALtot[1:r$I],
+                  R$s)
+molten <- melt(est,c(1,4),2:3)
+names(molten)[3:4] <- c("estimator","estimate")
+xyplot(estimate~simulated,molten,aspect=1,groups=estimator,auto.key=TRUE)
+xyplot(fortran~winbugs,est,aspect=1,groups=type,auto.key=TRUE,panel=function(...){panel.abline(0,1);panel.xyplot(...)})
+
+pdf("winbugs-fortran-disagree.pdf",h=8,w=8)
+dl(xyplot,est,fortran~winbugs,type,aspect=1,
+   panel=function(...){panel.abline(0,1);panel.xyplot(...)},
+   method="empty.grid",
+   main="2 programs yield different ancestral allele frequency estimates for the same model")
+dev.off()
+
+d <- est$fortran-est$winbugs
+den <- density(d)
+sorted <- sort(d)
+cutoff <- sorted[which.max(diff(sorted))]
+cutoff <- -min(d)
+densityplot(~fortran-winbugs,est,
+            aspect=1,groups=type,auto.key=TRUE,
+            panel=function(...){
+              panel.densityplot(...)
+              panel.abline(v=cutoff)
+  })
+histogram(~fortran-winbugs,est,n=100)
+est
