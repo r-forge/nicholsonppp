19f21e21396bc96364374fa0c8d06969d5939f36
tdhock <tdhock@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-08-21 15:49:53 +0000

    R hangs after model fit, and no printing from fortran
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/nicholsonppp@45 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/src/modeltest.R
+++ pkg/src/modeltest.R
@@ -1,5 +1,5 @@
 system("R CMD SHLIB fitnicholsonppp.f90")
 dyn.load("fitnicholsonppp.so")
 source("../R/model.R")
-nicholsonppp(matrix(rbeta(100,0.7,0.7)*100,nrow=20,ncol=5))
-
+result <- nicholsonppp(matrix(rbeta(100,0.7,0.7)*100,nrow=20,ncol=5))
+print(result)
