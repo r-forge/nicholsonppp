d2461926bf331e1af7d1ac61e5b77062f6582bb1
tdhock (Toby Dylan Hocking) <tdhock5@gmail.com>
2009-08-25 11:22:42 +0000

    remove writing file
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/nicholsonppp@49 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/model.R
+++ pkg/R/model.R
@@ -35,14 +35,19 @@ nicholsonppp <- function
  ){
   nmrk <- nrow(Y_OBS)
   npop <- ncol(Y_OBS)
-  PPPvec <- rep(2.5,nmrk)
+  return_ppp <- rep(-2.5,nmrk)
+  return_a <- rep(-2.5,nmrk*npop)
+  return_c <- rep(-2.5,npop)
+  return_p <- rep(-2.5,nmrk)
   N_OBS <- matrix(N_OBS,nrow=nrow(Y_OBS),ncol=ncol(Y_OBS))
   YY <- Y_OBS
   NN <- N_OBS
+  ## order of argument names here should match fortran code
   fargs <- list(integer=c("npop","nmrk","seed","nvaleurs","thin","burn_in",
                   "npilot","pilot_length","out_option","YY","NN"),
                 single=c("delta_a_init","delta_p_init","delta_c_init",
-                  "rate_adjust","acc_inf","acc_sup"))
+                  "rate_adjust","acc_inf","acc_sup",
+                  "return_ppp","return_a","return_c","return_p"))
   fc <- list(as.name(".Fortran"),"fitnicholsonppp")
   for(N in names(fargs))for(a in fargs[[N]]){
     fc[[L <- length(fc)+1]] <- call(paste("as.",N,sep=""),as.name(a))
@@ -51,10 +56,14 @@ nicholsonppp <- function
   names(fc)[2] <- ""
   cc <- as.call(fc)
   ## debugging:
-  print(cc)
-  print(sapply(cc,function(x)if(!is.character(x))eval(x) else x))
+  ##print(cc)
+  ##print(sapply(cc,function(x)if(!is.character(x))eval(x) else x))
   res <- eval(cc)
-  print("FOOBAR")
   ## post-process...
-  res
+  pre <- "return_"
+  rnames <- grep(pre,names(res),val=TRUE)
+  ret <- res[rnames]
+  names(ret) <- sub(pre,"",rnames)
+  ret$a <- matrix(ret$a,nrow=nmrk,ncol=npop)
+  ret
 }
