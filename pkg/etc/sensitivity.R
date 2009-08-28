9b3769daf579357a41ad8f76107e90b35fd858e4
tdhock <tdhock@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-08-28 12:22:35 +0000

    add sensitivity and c timecourse studies
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/nicholsonppp@66 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/etc/sensitivity.R
@@ -0,0 +1,24 @@
+load("c.Rdata")
+library(ggplot2)
+ppp <- sapply(res4,function(L)L$ppp)
+molt <- melt(ppp)
+names(molt) <- c("locus","generation","ppp")
+molt$generation <- molt$generation*25
+type=factor(c(rep("none",800),rep("positive",100),rep("balancing",100)))
+df <- data.frame(molt,type)
+library(lattice)
+densityplot(~ppp|generation,df,groups=type,layout=c(1,4))
+
+load("bugs/all.models.Rdata")
+ppp <- melt(cbind(old=R$ppp$PPPVALtot[1:1000],r1=fit1$ppp,r0.7=fit0.7$ppp))
+names(ppp) <- c("locus","program","ppp")
+df <- data.frame(ppp,type)
+densityplot(~ppp|program,df,groups=type,layout=c(1,3))
+densityplot(~ppp|program,df,layout=c(1,3))
+
+source("../R/sim.R")
+sims <- sapply(10^(-4:0),
+               function(s)sim.drift.selection(loci=100,
+                                              generations=100,
+                                              s=s),
+               simplify=FALSE)
